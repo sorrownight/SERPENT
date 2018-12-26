96a2a7d71d095e688b1d0f827ce2e8b7e6db54cb
sorrownight <34826771+sorrownight@users.noreply.github.com>
2018-12-25 16:27:44 -0800

    Public build: 1.0.0 Beta
    Added documentation
    Added a scoring system

--- src/GridData.java
+++ src/GridData.java
@@ -1,39 +1,92 @@
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
+/**
+ * Allows the construction of a (width)x(height) grid that spawns a Snake of (defaultLength).
+ * The Grid must be updated each time a change to its states occurs by calling the updateBoard() method.
+ *
+ * States of the game:
+ *          In Session: From start
+ *          Game Over: When the Snake hits itself or when it hits a Wall (an exception will be thrown for each case)
+ *
+ * Expectation when the game ends: no more calls to the updateBoard() method should be made
+ * Expectation when wanting to restart the game: a call to the restart() method should be made
+ *
+ * Features:
+ *          Possible queries to the length of the Snake (not counting the head) and to the location of the head can be
+ *          made.
+ *          The Food and Snake's spawning locations are randomized. The Food's spawning location is not restricted
+ *          except that it cannot spawn ON the Snake. The Snake's spawning location is restricted to the center tiles of
+ *          the Grid.
+ *
+ * Restrictions:
+ *          This class does not allow direct mutations to its states. All changes will occur within the Model
+ *          as to accredit the MVC pattern.
+ *          The object MUST be constructed, at minimum, a 2 x 2 grid.
+ *
+ * State expectations:
+ *          Two arrays will be allocated for the location of each tile of the Snake. It is expected that unused memories
+ *          be set (by default) to -1.
+ *
+ * @author Luan Ta
+ * @version 1.0.0 Beta
+ */
 public class GridData
 {
-    private BoardState[][] board;
+    private BoardState[][] board; // The 2D Grid of the game
     private GraphicsContext gc;
-    private int snakeLength;
-    private int[] bodyLocX;
+    private int snakeLength; // The length of the Snake (not counting the Head)
+    private int[] bodyLocX; // The location of each tile of the the Snake's body
     private int[] bodyLocY;
-    private int locX;
+    private int locX; // Head location
     private int locY;
-    private int prevLocX;
+    private int prevLocX; // The previous location of the Head after one execution cycle
     private int prevLocY;
-    private int foodLocX;
+    private int foodLocX; // The location of the Food
     private int foodLocY;
-    public static final int DIMENSION = 30;
+    public static final int DIMENSION = 30; // The default Dimension of each Graphic object
 
+    /**
+     * Construct a Grid of the dimension width x height and spawn a Snake with the length of defaultLength
+     * @param width the width of the Grid
+     * @param height the height of the Grid
+     * @param defaultLength the starting length of the Snake
+     * @param gc the Graphics object to which this object will be displayed upon
+     * @throws IllegalArgumentException if the defaultLength is less than 1 or if the width/height is less than 2
+     */
     public GridData(int width, int height, int defaultLength, GraphicsContext gc)
     {
+        /*
+        The game requires AT LEAST a 2 x 2 grid to function
+         */
         if (defaultLength < 1 || width < 2 || height < 2)
             throw new IllegalArgumentException();
 
+        /*
+        The maximum length for the Snake's body is the number of tiles of the Grid
+         */
         bodyLocX = new int[width*height];
         bodyLocY = new int[width*height];
 
+        /*
+        Initialize the arrays
+         */
         for (int i = 0; i < bodyLocX.length; i++)
         {
             bodyLocX[i] = -1;
             bodyLocY[i] = -1;
         }
 
+        /*
+        Initialize the instance variables
+         */
         snakeLength = defaultLength;
         board = new BoardState[width][height];
         this.gc = gc;
 
+        /*
+        Algorithm for the randomized spawning location of the Snake
+         */
         locX =(int) (Math.random()*width/2)+width/4;
         locY =(int) (Math.random()*height/2)+height/4;
 
@@ -64,7 +117,7 @@ public class GridData
 
     public int getSnakeLength()
     {
-        return snakeLength+1;
+        return snakeLength;
     }
 
     public void determineMove(int direction)
@@ -151,27 +204,27 @@ public class GridData
         {
             for (int column = 0; column < board[row].length; column++)
             {
-                gc.strokeRect(row*DIMENSION,column*DIMENSION,30,30);
+                gc.strokeRect(row*DIMENSION,column*DIMENSION,DIMENSION,DIMENSION);
                 switch (board[row][column])
                 {
                     case EMPTY:
                         gc.setFill(Color.WHITE);
-                        gc.fillRect(row*DIMENSION,column*DIMENSION,30,30);
+                        gc.fillRect(row*DIMENSION,column*DIMENSION,DIMENSION,DIMENSION);
                         break;
 
                     case FOOD:
                         gc.setFill(Color.BLUE);
-                        gc.fillRect(row*DIMENSION,column*DIMENSION,30,30);
+                        gc.fillRect(row*DIMENSION,column*DIMENSION,DIMENSION,DIMENSION);
                         break;
 
                     case BODY:
                         gc.setFill(Color.BLACK);
-                        gc.fillRect(row*DIMENSION,column*DIMENSION,30,30);
+                        gc.fillRect(row*DIMENSION,column*DIMENSION,DIMENSION,DIMENSION);
                         break;
 
                     case HEAD:
                         gc.setFill(Color.RED);
-                        gc.fillRect(row*DIMENSION,column*DIMENSION,30,30);
+                        gc.fillRect(row*DIMENSION,column*DIMENSION,DIMENSION,DIMENSION);
                         break;
                 }
             }
@@ -199,16 +252,16 @@ public class GridData
 
     private void newFood()
     {
-        foodLocX = (int) (Math.random()*board.length/2);
-        foodLocY = (int) (Math.random()*board[0].length/2);
+        foodLocX = (int) (Math.random()*board.length);
+        foodLocY = (int) (Math.random()*board[0].length);
 
         /*
         If the spawn location of the food and the head are the same, change the location of the food
          */
         while (board[foodLocX][foodLocY] == BoardState.HEAD || board[foodLocX][foodLocY] == BoardState.BODY)
         {
-            foodLocX = (int) (Math.random()*board.length/2);
-            foodLocY = (int) (Math.random()*board[0].length/2);
+            foodLocX = (int) (Math.random()*board.length);
+            foodLocY = (int) (Math.random()*board[0].length);
         }
     }
 
--- src/SerpentApp.java
+++ src/SerpentApp.java
@@ -12,17 +12,23 @@ import java.util.Optional;
 import java.util.Timer;
 import java.util.TimerTask;
 
+/**
+ * A simple Snake game written using the application of JavaFX
+ *
+ * @author Luan Ta
+ * @version 1.0.0 Beta
+ */
 public class SerpentApp extends Application
 {
     private Scene main; // The reference to the program's display
     private GridPane root;
-    private GridData board;
-    private long stepTiming;
-    private int direction;
-    private static final int STARTING_DIRECTION = 0; // LEFT
+    private GridData board; // The Model of this program. Holds the states of the game.
+    private long stepTiming; // The timing between each execution. Intended to restrict/delay the input of another key
+    private int direction; // Based on the input of the user, determine the direction of the Snake
+    private static final int STARTING_DIRECTION = 0; // Starting from the Left. See (*) below
     public static final int DELAY = 300; // In milliseconds
     private Timer timer;
-    private boolean inSession;
+    private boolean inSession; // Determines whether the game is still in session
 
     public static void main(String[] args)
     {
@@ -32,6 +38,9 @@ public class SerpentApp extends Application
     @Override
     public void start(Stage primaryStage)
     {
+        /*
+        Initialize instance variables and set up the layout
+         */
         stepTiming = System.currentTimeMillis();
         direction = STARTING_DIRECTION;
 
@@ -40,6 +49,7 @@ public class SerpentApp extends Application
         root = new GridPane();
 
         Label gameState = new Label();
+
         gameState.setText("Game is in Session!");
 
         Canvas canvas = new Canvas(600,600);
@@ -53,10 +63,20 @@ public class SerpentApp extends Application
         main = new Scene(root,1250,1000); // Set a fixed size for the Application
         primaryStage.setResizable(false);
 
+        Label score = new Label();
+        score.setText("Current Snake Length: "+ board.getSnakeLength());
+
+        root.add(score,2,0);
+
+        /*
+        Create a Controller unit for the layout that listens to 2 Key inputs:
+        Left: to change the direction to the Left of the current direction
+        Right: to change the direction to the Right of the current direction
+         */
         main.setOnKeyPressed(event ->
         {
             /*
-            If the time of the last execution of a step is larger than the current time minus a delay constant,
+            If the time of the last execution of a step is smaller than the current time minus a delay constant,
             DO NOT execute this key command.
              */
             if (stepTiming < System.currentTimeMillis() - DELAY - 50)
@@ -71,13 +91,17 @@ public class SerpentApp extends Application
                         direction++;
                         break;
                 }
+                /*
+                If the direction has gone one full circle clockwise or counter-clockwise, re-initiate the direction
+                as appropriate
+                 */
                 if (direction > 3)
                     direction = 0;
 
                 else if (direction < 0)
                     direction = 3;
 
-                stepTiming = System.currentTimeMillis();
+                stepTiming = System.currentTimeMillis(); // Update the timing of this execution
             }
         });
 
@@ -88,24 +112,36 @@ public class SerpentApp extends Application
 
         timer = new Timer();
 
+        /*
+        Create a concurrent thread that execute and update the game Grid as well as ending such as necessary.
+         */
         TimerTask task = new TimerTask()
         {
             @Override
             public void run()
             {
-                if (!inSession)
+                if (!inSession) // If the game is over, cancel the task and cease updating
                     timer.cancel();
 
-                board.determineMove(direction);
+                board.determineMove(direction); // Using the user's input, determine the next state of the Snake
                 try
                 {
-                    board.updateBoard();
+                    board.updateBoard(); // Update the board and its states
+                    Platform.runLater(() ->
+                            score.setText("Current Snake Length: "+ board.getSnakeLength())); // Update the score
                 }
+                /*
+                If the Snake hits itself, GameOverException will be thrown
+                If it hits a wall, ArrayIndexOutOfBoundsException will be thrown
+                 */
                 catch (GameOverException | ArrayIndexOutOfBoundsException e)
                 {
-                    inSession = false;
+                    inSession = false; // Indicate that the game is over
                     Platform.runLater(() ->
                     {
+                        /*
+                        Display an Alert as well as changing a Label
+                         */
                         gameState.setText("Game is Over!");
                         Alert overAlert = new Alert(Alert.AlertType.CONFIRMATION);
 
@@ -118,16 +154,15 @@ public class SerpentApp extends Application
 
                         overAlert.setContentText("Restart the Game?");
                         Optional<ButtonType> result = overAlert.showAndWait();
+
                         if (result.get() == ButtonType.OK)
                         {
                             restartTimer();
-
-
                             gameState.setText("Game is in Session");
                         }
                         else
                         {
-
+                            // TODO: Add a way to restart the game
                         }
                     });
                 }
